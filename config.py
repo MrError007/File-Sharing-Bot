19081d4716422ce5e7bded208650a530dcdac9cd
MrError007 <141121641+MrError007@users.noreply.github.com>
2023-09-24 12:37:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ APP_ID = int(os.environ.get("APP_ID", "11071959"))
 API_HASH = os.environ.get("API_HASH", "1d5fcbb310ce34f4f767a1c6d5148beb")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001924325585"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001823249514"))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "5810898650"))
